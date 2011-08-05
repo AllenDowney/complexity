b6c7f875b0eab9fa0c8c683e6025a346cba337e0
allendowney <allendowney@9420ce47-5017-383e-dfcd-09c6d12e7165>
2011-08-05 20:22:42 +0000

    Revising Chapter 4

--- trunk/code/Zipf.py
+++ trunk/code/Zipf.py
@@ -1,61 +1,57 @@
+""" Code example from Complexity and Computation, a book about
+exploring complexity science with Python.  Available free from
 
-"""
+http://greenteapress.com/complexity
 
-Code example from _Computational_Modeling_
-http://greenteapress.com/compmod
-
-Copyright 2008 Allen B. Downey.
+Copyright 2011 Allen B. Downey.
 Distributed under the GNU General Public License at gnu.org/licenses/gpl.html.
-
 """
 
 import sys
 import string
-import pylab
 
-from Hist import *
+import matplotlib.pyplot as pyplot
 
-class Zipf(Hist):
-    """Zipf is a histogram that maps from words to frequencies.
+import Pmf
 
-    It provides methods to generate data for a Zipf plot (frequency
-    versus rank).
+
+def rank_freq(hist):
+    """Returns a list of tuples where each tuple is a rank
+    and the number of times the item with that rank appeared.
     """
-    def rank_freq(self):
-        """return a list of tuples where each tuple is a rank
-        and the number of times the item with that rank appeared.
-        """
-        # sort the list of frequencies in decreasing order
-        t = [f[0] for f in self.itervalues()]
-        t.sort(reverse=True)
+    # sort the list of frequencies in decreasing order
+    freqs = hist.Freqs()
+    freqs.sort(reverse=True)
 
-        # enumerate the ranks and frequencies 
-        rf = [(r+1, f) for r, f in enumerate(t)]
-        return rf
+    # enumerate the ranks and frequencies 
+    rf = [(r+1, f) for r, f in enumerate(freqs)]
+    return rf
 
 
-    def print_ranks(self):
-        """print the rank vs. frequency data"""
-        for r, f in self.rank_freq():
-            print r, f
+def print_ranks(hist):
+    """Prints the rank vs. frequency data."""
+    for r, f in rank_freq(hist):
+        print r, f
 
-    def plot_ranks(self):
-        """use pylab to plot a chart in which the x-axis
-        shows ranks in increasing order and the y-axis shows the
-        frequency of the value with the given rank.
-        """
-        t = self.rank_freq()
-        rs, fs = zip(*t)
-        pylab.loglog(rs, fs, 'r-')
-        pylab.xlabel('log ranks')
-        pylab.ylabel('log frequencies')
-        pylab.show()
+
+def plot_ranks(hist, scale='log'):
+    """Plots frequency vs. rank."""
+    t = rank_freq(hist)
+    rs, fs = zip(*t)
+
+    pyplot.clf()
+    pyplot.xscale(scale)
+    pyplot.yscale(scale)
+    pyplot.title('Zipf plot')
+    pyplot.xlabel('rank')
+    pyplot.ylabel('frequency')
+    pyplot.plot(rs, fs, 'r-')
+    pyplot.show()
 
 
 def iter_words(filename):
-    """this is a generator that returns an iterator that yields
-    the words from a file one at a time, with the punctuation stripped."""
-
+    """A generator that yields the words from a file one at a time,
+    with the punctuation stripped."""
     fp = open(filename, 'r')
     for line in fp:
         line = line.replace('--', ' ')
@@ -69,15 +65,15 @@ def main(name, filename='gatsby.txt', flag='print', *args):
 
     # build a Zipf histogram of the words in filename
     iter = iter_words(filename)
-    z = Zipf(iter)
+    hist = Pmf.MakeHistFromList(iter)
 
     # either print the results or plot them
     if flag == 'print':
-        z.print_ranks()
+        print_ranks(hist)
     elif flag == 'plot':
-        z.plot_ranks()
+        plot_ranks(hist)
     else:
-        print 'Usage: Zipf.py [print|plot]'
+        print 'Usage: Zipf.py filename [print|plot]'
 
 
 if __name__ == '__main__':
