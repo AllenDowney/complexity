617709ea66a9bf2227496a3f8aa1f6c06db6a917
wingless.angel31@gmail.com <wingless.angel31@gmail.com@9420ce47-5017-383e-dfcd-09c6d12e7165>
2012-01-13 18:06:25 +0000

    normsGame.py tweaked to facilitate exercise

--- code/normsGame.py
+++ code/normsGame.py
@@ -1,35 +1,30 @@
-"""This file contains code for the Norms Game case study in
-Think Complexity, by Allen B. Downey.
+"""Molly Grossman, Mandy Korpusik, Philip Loh"""
 
-Authors: Molly Grossman, Mandy Korpusik, Philip Loh
-
-License:
-"""
-
-
-"""Definitions:
-
+"""Definitions
 world = whole population
 player = each player
 boldness = constant of probability for cheating
 vengefulness = constant of probability for punishing
-fitness = measurement of fitness
+fitness = measurement of fitness"""
 
-"""
+import sys, random, math
 
-import sys
-import random
-import math
-
-"""Parameters of the simulation"""
-R = 3   # Reward from cheating
-P = -9  # Punishment
-C = -2  # Cost of punishing
-D = -1  # Damage from each offense
+"""Constant World-wide variables"""
+R = 3   #Reward from cheating
+P = 9  #Punishment
+C = 2  #Cost of punishing
+D = 1  #Damage from each offense
 MAXBV = 7.0 #must be power of 2, less 1
 
 if int(math.log((MAXBV + 1), 2)) != math.log((MAXBV + 1), 2):
     raise ValueError("MAXBV MUST BE A FLOAT AND A POWER OF 2, LESS 1")
+
+class Stats(object):
+    def __init__(self):
+        self.stableBV = []
+        
+gameStats = Stats()
+
 
 class Player(object):
     def __init__(self, idNum, b, v):
@@ -37,14 +32,14 @@ class Player(object):
         self.boldness = b
         self.vengefulness = v
         self.fitness = 0
+        return
 
     def __str__(self):
-        s = 'id {0:2} boldness {1:.2f} vengefulness {2:.2f} fitness {3:3}'
-        return s.format(self.idNum, self.boldness, 
-                        self.vengefulness, self.fitness)
+        return "id {0:2} boldness {1:.2f} vengefulness {2:.2f} fitness {3:3}".format(self.idNum, self.boldness, self.vengefulness, self.fitness)
 
     def update(self, deltaHealth):
         self.fitness += deltaHealth
+        return
 
     def clone(self):
         return Player(self.idNum, self.boldness, self.vengefulness)
@@ -66,17 +61,19 @@ class Player(object):
 
     def setBoldness(self, b):
         self.boldness = int(b)
+        return
 
     def setVengefulness(self, v):
         self.vengefulness = int(v)
+        return
 
     def resetHealth(self):
         self.fitness = 0
-
+        return
 
 class World(set):
     def __init__(self, N = 20):
-        """Begins World with 100 Persons by default"""
+        """begins World with 100 Persons by default"""
         self.population = 20
         for idNum in range(N):
             """random values near half"""
@@ -89,12 +86,12 @@ class World(set):
             v = min(int(MAXBV), max(0, v0))
             
             self.add(Player(idNum, b, v))
+        return
 
     def __str__(self):
         hMean, hStDev = self.healthMeanStDev()
         bMean, vMean = self.bvMean()
-        s = 'boldness {0:.2f} vengefulness {1:.2f} h {2:3}'
-        return s.format(bMean, vMean, hMean)
+        return "boldness {0:.2f} vengefulness {1:.2f} h {2:3}".format(bMean, vMean, hMean)
 
     def printDetails(self):
         s = ""
@@ -105,41 +102,35 @@ class World(set):
     def clone_update(self, persons):
         for person in persons:
             self.add(person.clone())
+        return
 
     def _gameCheat(self, pbs):
-        """First game: cheatORnot"""
+        """first game: cheatORnot"""
         cheaters = set()
         for person in self:
             if person.getBoldness() >= pbs:
-                # Reward plus negative damage because we levy damage 
-                # on everyone as a shortcut
-                person.update(R - D) 
+                person.update(R + D) # Reward plus what would be taken out as damage because we levy damage on everyone as a shortcut
                 cheaters.add(person)
-
         for person in self:
-            person.update(len(cheaters) * D)
-
+            person.update(-len(cheaters) * D)
         return cheaters
 
     def _gamePunish(self, pbs, cheaters):
-        """Second game: punishORnot"""
+        """second game: punishORnot"""
         for cheater in cheaters:
             for observer in self - set([cheater]):
                 if(slt(pbs) and slt(observer.getVengefulness())):
-                    observer.update(C) # Cost of punishing
-                    cheater.update(P) # Punishment
+                    observer.update(-C) # Cost of punishing
+                    cheater.update(-P) # Punishment
+        return
 
     def gameSteps(self, steps=4):
-        """each step has two games: cheatORnot and punishORnot
-        
-        steps: number of rounds per generation
-        """
+        """each step has two games: cheatORnot and punishORnot; default 4 steps"""
         for step in range(steps):
-            # probability of being seen (same for each person in the 
-            # given round)
-            pbs = random.random() 
+            pbs = random.random() # probability of being seen (same for each person in the given round)
             cheaters = self._gameCheat(pbs)
             self._gamePunish(pbs, cheaters)
+        return
 
     def _healthMean(self):
         """returns mean fitness in world"""
@@ -176,14 +167,10 @@ class World(set):
         p_mutation = 0.01
         for person in self:
             xorstr = ''
-            # power is the power of two analog for MAXBV
-            power = int(MAXBV + 1)                      
-            vengefulness = person.getVengefulnessInt()  
-            boldness = person.getBoldnessInt()          
-
-            # power really needs to be a power of 2; safeguarding it 
-            # not being a power of two, we ceil the log2
-            numBits = 2 * int(math.log(power, 2))       
+            power = int(MAXBV + 1)                      # power is the power of two analog for MAXBV
+            vengefulness = person.getVengefulnessInt()  # vengefulness in int
+            boldness = person.getBoldnessInt()          # boldness in int
+            numBits = 2 * int(math.log(power, 2))       # power really needs to be a power of 2, safeguarding it not being a power of two, we ceil the log2
             for i in range(numBits):
                 xorstr += str(int(slt(p_mutation)))
             xorstr = int(xorstr, 2)
@@ -191,6 +178,7 @@ class World(set):
             newVals = xorstr ^ valsToXor
             person.setBoldness(int(newVals / 8))
             person.setVengefulness(int(newVals % 8))
+        return
     
     def repopulate(self):
         """at every generation, repopulate"""
@@ -204,30 +192,55 @@ class World(set):
                 newWorld.add(person)
             if health >= (mu + sigma):
                 newWorld.add(person.clone())
-
-        n = self.population - len(newWorld)
-        if n > 0:
-            newWorld.clone_update(random.sample(self, n))
-        elif n < 0:
-            newWorld.difference_update(random.sample(newWorld, -n))
-
-        assert len(newWorld) == self.population
+        if len(newWorld) < self.population:
+            newWorld.clone_update(random.sample(self, self.population - len(newWorld)))
+        elif len(newWorld) > self.population:
+            newWorld.difference_update(random.sample(newWorld, len(newWorld) - self.population))
+        assert(len(newWorld) == self.population)
         self.clear()
         self.update(newWorld)
-
+        return
 
 def slt(prob):
     """set less than"""
-    return random.random() < prob
+    return (random.random() < prob)
 
+def plotStableBV(show=True):
+    import matplotlib.pyplot as pyplot
+    
+    stableB, stableV = zip(*gameStats.stableBV)
+    pyplot.scatter(stableB, stableV, alpha=0.7, label="B, V after 300 Generations")
+    pyplot.title("Boldness, Vengefulness after 300 Generations")
+    pyplot.xlabel("Boldness")
+    pyplot.ylabel("Vengefulness")
+    pyplot.axis([-0.1, 1.1, -0.1, 1.1])
+    if show: pyplot.show()
+    return
+
+def runReplicate(trials=100):
+    goodB = 0
+    print "R {0} P {1} C {2} D {3}".format(R, P, C, D)
+    for trial in range(trials):
+        world = World()
+        sys.stdout.write('.')
+        
+        for generation in range(300):
+            world.gameSteps()
+            world.mutate()
+            world.repopulate()
+
+        gameStats.stableBV.append(world.bvMean())
+        goodB += 1 if world.bvMean()[0] <= 0.4 else 0
+
+    sys.stdout.write('\n')
+    
+    return goodB/float(trials)
 
 def main(script, *args):
-    world = World()
-        
-    for generation in range(300):
-        world.gameSteps()
-        world.mutate()
-        world.repopulate()
+    p = runReplicate()
+    print "Probability of good outcome", p
+    plotStableBV()
+    return
 
 if __name__ == "__main__":
     main(*sys.argv)
